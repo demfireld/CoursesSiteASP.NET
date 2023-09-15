e4dd19d8a41c4316bad998cc5168416211bcc975
nazar <nazar@COMPUTER>
2023-09-15 20:55:35 +0300

    Dependency Injection. Added realization of interfaces.

---
+++ courses/Repository/CategoriesRepository.cs
@@ -0,0 +1,51 @@
+﻿using courses.DataBase;
+using courses.Interfaces;
+using courses.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace courses.Repository
+{
+    public class CategoriesRepository : ICategoriesRepository
+    {
+        private readonly ApplicationDbContext _context;
+
+        public CategoriesRepository(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public bool Add(Categories categories)
+        {
+            _context.Categories.Add(categories);
+            return Save();
+        }
+
+        public bool Delete(Categories categories)
+        {
+            _context.Categories.Remove(categories);
+            return Save();
+        }
+
+        public async Task<IEnumerable<Categories>> GetAll()
+        {
+            return await _context.Categories.ToListAsync();
+        }
+
+        //public async Task<Categories> GetByIdAsync(int id)
+        //{
+        //    throw new NotImplementedException();
+        //}
+
+        public bool Save()
+        {
+            var isSaved = _context.SaveChanges();
+            return isSaved > 0 ? true : false;
+        }
+
+        public bool Update(Categories categories)
+        {
+            _context.Update(categories);
+            return Save();
+        }
+    }
+}
---
+++ courses/Repository/CoursesRepository.cs
@@ -0,0 +1,51 @@
+﻿using courses.DataBase;
+using courses.Interfaces;
+using courses.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace courses.Repository
+{
+    public class CoursesRepository : ICoursesRepository
+    {
+        private readonly ApplicationDbContext _context;
+
+        public CoursesRepository(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public bool Add(Courses courses)
+        {
+            _context.Courses.Add(courses);
+            return Save();
+        }
+
+        public bool Delete(Courses courses)
+        {
+            _context.Courses.Remove(courses);
+            return Save();
+        }
+
+        public async Task<IEnumerable<Courses>> GetAll()
+        {
+            return await _context.Courses.ToListAsync();
+        }
+
+        //public async Task<Courses> GetByIdAsync(int id)
+        //{
+        //    throw new NotImplementedException();
+        //}
+
+        public bool Save()
+        {
+            var isSaved = _context.SaveChanges();
+            return isSaved > 0 ? true : false;
+        }
+
+        public bool Update(Courses courses)
+        {
+            _context.Update(courses);
+            return Save();
+        }
+    }
+}
