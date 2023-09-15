32eaed2c9cf22067c17edc0e2d61e0d4f57b1385
nazar <nazar@COMPUTER>
2023-09-15 20:53:55 +0300

    Dependency Injection. Interfaces added.

---
+++ courses/Interfaces/ICategoriesRepository.cs
@@ -0,0 +1,15 @@
+﻿using courses.Models;
+
+namespace courses.Interfaces
+{
+    public interface ICategoriesRepository
+    {
+        Task<IEnumerable<Categories>> GetAll();
+       // Task<Categories> GetByIdAsync(int id);
+
+        bool Add(Categories categories);
+        bool Update(Categories categories);
+        bool Delete(Categories categories);
+        bool Save();
+    }
+}
---
+++ courses/Interfaces/ICoursesRepository.cs
@@ -0,0 +1,15 @@
+﻿using courses.Models;
+
+namespace courses.Interfaces
+{
+    public interface ICoursesRepository
+    {
+        Task<IEnumerable<Courses>> GetAll();
+        //Task<Courses> GetByIdAsync(int id);
+
+        bool Add(Courses courses);
+        bool Update(Courses courses);
+        bool Delete(Courses courses);
+        bool Save();
+    }
+}
