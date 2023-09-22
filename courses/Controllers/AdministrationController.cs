9e7a64b8741bb6c2bb46ecf4bd3da9a02a68cbb5
nazar <nazar@COMPUTER>
2023-09-22 19:36:04 +0300

    Added the new page and controller of administration

---
+++ courses/Controllers/AdministrationController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace courses.Controllers
+{
+    [Authorize(Roles = "admin,courses editor,news editor,teacher")]
+    public class AdministrationController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
