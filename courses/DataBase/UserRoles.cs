e42d0eff1fd0fe7ce9563ec9db6f7546f5669549
nazar <nazar@COMPUTER>
2023-09-22 19:34:07 +0300

    Added the new roles and has been deleted role of the "Teachers"

--- courses/DataBase/UserRoles.cs
+++ courses/DataBase/UserRoles.cs
@@ -2,7 +2,10 @@
 {
     public static class UserRoles
     {
-        public const string Admin = "admin";
         public const string User = "user";
+        public const string Admin = "admin";
+        public const string Teacher = "teacher";
+        public const string CoursesEditor = "courses editor";
+        public const string NewsEditor = "news editor";
     }
 }
