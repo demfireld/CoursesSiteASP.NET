2af80bfeb278c4c4e9b6ea0c59ac5b083c9f3b6e
demfireld <gamew5177@gmail.com>
2023-09-26 02:28:01 +0300

    Minor changes

--- courses/Controllers/HomeController.cs
+++ courses/Controllers/HomeController.cs
@@ -2,21 +2,30 @@
 using courses.Models;
 using courses.ViewModels;
 using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
 
 namespace courses.Controllers
 {
     public class HomeController : Controller
     {
         private readonly List<Courses> _courses;
+        private readonly ILogger<HomeController> _logger;
 
-        public HomeController(ApplicationDbContext context)
+        public HomeController(ILogger<HomeController> logger)
         {
-            _courses = context.Courses.ToList();
+            _logger = logger;
         }
+
         public IActionResult Index()
         {
             HomeViewModel homeViewModel = new HomeViewModel { Courses = _courses };
             return View(homeViewModel);
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
     }
 }
