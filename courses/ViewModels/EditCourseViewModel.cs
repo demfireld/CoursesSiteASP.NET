ffccc6f76a236a280cae9894a4e8af1572e582b4
demfireld <gamew5177@gmail.com>
2023-09-25 20:01:44 +0300

    Added EditCourseViewModel

---
+++ courses/ViewModels/EditCourseViewModel.cs
@@ -0,0 +1,22 @@
+﻿using courses.Models;
+
+namespace courses.ViewModels
+{
+    public class EditCourseViewModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public string ShortDescription { get; set; }
+
+        public string LongDescription { get; set; }
+
+        public string Img { get; set; }
+
+        public double Price { get; set; }
+
+        public int CategoryId { get; set; }
+
+        public IEnumerable<Categories>? Category { get; set; }
+    }
+}
