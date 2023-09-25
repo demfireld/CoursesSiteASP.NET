4a8fce87e95160243324d100f5d369b9e981d3dd
demfireld <gamew5177@gmail.com>
2023-09-25 19:54:30 +0300

    Two methods have been added in the administration controller

--- courses/Controllers/AdministrationController.cs
+++ courses/Controllers/AdministrationController.cs
@@ -69,5 +69,50 @@ namespace courses.Controllers
             _coursesRepository.Add(courses);
             return RedirectToAction("Index");
         }
+
+        [Authorize(Roles = "admin,courses editor")]
+        [HttpGet]
+        public async Task<IActionResult> EditCourse(int id)
+        {
+            IEnumerable<Categories> categories = await _categoriesRepository.GetAll();
+
+            Courses course = await _coursesRepository.GetByIdAsync(id);
+            if (course == null) { return View("Error"); }
+
+            EditCourseViewModel EditCourseViewModel = new EditCourseViewModel
+            {
+                Name = course.Name,
+                ShortDescription = course.ShortDescription,
+                LongDescription = course.LongDescription,
+                Img = course.Img,
+                Price = course.Price,
+                CategoryId = course.CategoryId,
+                Category = categories
+            };
+            return View(EditCourseViewModel);
+        }
+
+        [HttpPost]
+        public IActionResult EditCourse(int id, EditCourseViewModel editCourseViewModel)
+        {
+            if (!ModelState.IsValid)
+            {
+                ModelState.AddModelError("", "Ошибка редактирования курса");
+                return View("EditCourse", editCourseViewModel);
+            }
+
+            Courses course = new Courses()
+            {
+                Id = id,
+                Name = editCourseViewModel.Name,
+                ShortDescription = editCourseViewModel.ShortDescription,
+                LongDescription = editCourseViewModel.LongDescription,
+                Img = editCourseViewModel.Img,
+                Price = editCourseViewModel.Price,
+                CategoryId = editCourseViewModel.CategoryId,
+            };
+            _coursesRepository.Update(course);
+            return RedirectToAction("Index");
+        }
     }
 }
