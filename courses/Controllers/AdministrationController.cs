404bff5c3d715cac9615cfe0675152560fd105ce
nazar <nazar@COMPUTER>
2023-09-25 13:18:03 +0300

    Third update administration

--- courses/Controllers/AdministrationController.cs
+++ courses/Controllers/AdministrationController.cs
@@ -28,8 +28,11 @@ namespace courses.Controllers
         }
 
         [Authorize(Roles = "admin")]
+        [HttpGet]
         public IActionResult AllUsers() => View(_userManager.Users.ToList());
 
+        [Authorize(Roles = "admin")]
+        [HttpGet]
         public async Task<IActionResult> AllCourses()
         {
             IEnumerable<Courses> courses = await _coursesRepository.GetAll();
@@ -41,6 +44,38 @@ namespace courses.Controllers
             };
 
             return View(allCoursesViewModel);
+        }
+
+        [Authorize(Roles = "admin,courses editor")]
+        [HttpGet]
+        public async Task<IActionResult> CreateCourse()
+        {
+            IEnumerable<Categories> categories = await _categoriesRepository.GetAll();
+
+            CreateCoursesViewModel createCoursesViewModel = new CreateCoursesViewModel()
+            {
+                Category = categories
+            };
+            return View(createCoursesViewModel);
+        }
+
+        [HttpPost]
+        public IActionResult CreateCourse(CreateCoursesViewModel createCoursesViewModel)
+        {
+            if (ModelState.IsValid)
+            {
+                Courses courses = new Courses()
+                {
+                    Name = createCoursesViewModel.Name,
+                    ShortDescription = createCoursesViewModel.ShortDescription,
+                    LongDescription = createCoursesViewModel.LongDescription,
+                    Img = createCoursesViewModel.Img,
+                    Price = createCoursesViewModel.Price,
+                    CategoryId = createCoursesViewModel.CategoryId
+                };
+                _coursesRepository.Add(courses);
+            }
+            return RedirectToAction("Index");
         }
     }
 }
