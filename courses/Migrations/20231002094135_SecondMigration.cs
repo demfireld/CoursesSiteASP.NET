d9be9e4f9803e555f83dcd93ebc8796059323134
nazar <nazar@COMPUTER>
2023-10-02 16:45:44 +0300

    Update the migrations

---
+++ courses/Migrations/20231002094135_SecondMigration.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace courses.Migrations
+{
+    /// <inheritdoc />
+    public partial class SecondMigration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Name",
+                table: "Courses",
+                newName: "Title");
+
+            migrationBuilder.RenameColumn(
+                name: "CategoryName",
+                table: "Categories",
+                newName: "CategoryTitle");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Title",
+                table: "Courses",
+                newName: "Name");
+
+            migrationBuilder.RenameColumn(
+                name: "CategoryTitle",
+                table: "Categories",
+                newName: "CategoryName");
+        }
+    }
+}
