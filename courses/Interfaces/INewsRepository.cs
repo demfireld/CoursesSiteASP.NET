8ac82377c4f184cf1c0b4de8c20a37dd285c82ea
nazar <nazar@COMPUTER>
2023-10-02 16:47:58 +0300

    Added the new interface and his realization

---
+++ courses/Interfaces/INewsRepository.cs
@@ -0,0 +1,14 @@
+﻿using courses.Models;
+
+namespace courses.Interfaces
+{
+    public interface INewsRepository
+    {
+        Task<IEnumerable<News>> GetAllAsync();
+        Task<News> GetByIdAsync(int id);
+        bool Add(News news);
+        bool Update(News news);
+        bool Delete(News news);
+        bool Save();
+    }
+}
---
+++ courses/Repository/NewsRepository.cs
@@ -0,0 +1,52 @@
+﻿using courses.DataBase;
+using courses.Interfaces;
+using courses.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace courses.Repository
+{
+    public class NewsRepository : INewsRepository
+    {
+        private readonly ApplicationDbContext _context;
+
+        public NewsRepository(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        
+        public async Task<IEnumerable<News>> GetAllAsync()
+        {
+            return await _context.News.ToListAsync();
+        }
+
+        public async Task<News> GetByIdAsync(int id)
+        {
+            return await _context.News.FirstOrDefaultAsync(i => i.Id == id);
+        }
+
+        public bool Add(News news)
+        {
+            _context.News.Add(news);
+            return Save();
+        }
+
+        public bool Delete(News news)
+        {
+            _context.News.Remove(news);
+            return Save();
+        }
+
+
+        public bool Save()
+        {
+            var isSaved = _context.SaveChanges();
+            return isSaved > 0 ? true : false;
+        }
+
+        public bool Update(News news)
+        {
+            _context.Update(news);
+            return Save();
+        }
+    }
+}
